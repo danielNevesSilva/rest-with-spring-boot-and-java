1027704f4b5dbb399ebaea4d0e3bc5bff364c538
Daniel Neves <neves2590@gmail.com>
2023-08-01 11:29:53 -0300

    Tratamento de erro para as badrequest

--- src/main/java/br/com/eudio/apigateway/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/br/com/eudio/apigateway/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,7 @@
 package br.com.eudio.apigateway.handler;
 
 import br.com.eudio.apigateway.exceptions.ExceptionResponse;
-import br.com.eudio.apigateway.exceptions.UnsupportedMathOperationException;
+import br.com.eudio.apigateway.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -27,14 +27,14 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
     }
 
-    @ExceptionHandler(UnsupportedMathOperationException.class)
-    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundRequestExceptions(
             Exception ex, WebRequest request){
         ExceptionResponse exceptionResponse = new ExceptionResponse(
                 new Date(),
                 ex.getMessage(),
                 request.getDescription(false));
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 
     }
 
