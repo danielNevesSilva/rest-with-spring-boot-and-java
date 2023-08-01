0f10d60acf46e603a3bc606ff096920594e63cac
Daniel Neves <neves2590@gmail.com>
2023-08-01 11:05:58 -0300

    Criação da migration com flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `firt_name`, `gender`, `last_name`) VALUES
+	(2, 'Socorro - são paulo - Brasil', 'Kemilly', 'fame', 'Almeida'),
+	(3, 'Libano', 'Muhammad', 'mame', 'Ali'),
+	(4, 'Libano', 'Muhammad', 'mame', 'Ali'),
+	(5, 'Brazil', 'Daniel', 'mame', 'Neves'),
+	(6, 'Brazil Bahia', 'Leandro', 'fame', 'Tesla');
