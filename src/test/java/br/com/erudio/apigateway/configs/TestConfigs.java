658ae93b2e6abb8473ee05aeb4e7c525cca65a7a
Daniel Neves <neves2590@gmail.com>
2023-08-08 16:32:31 -0300

    Testando a Aplicação de CORS com diferentes Origins

--- src/test/java/br/com/erudio/apigateway/configs/TestConfigs.java
+++ src/test/java/br/com/erudio/apigateway/configs/TestConfigs.java
@@ -3,4 +3,13 @@ package br.com.erudio.apigateway.configs;
 public class TestConfigs {
 
     public static final int SERVER_PORT = 8888;
+    public static final String HEADER_PARAM_ORIGIN = "Origin";
+    public static final String HEADER_PARAM_AUTHORIZATION = "Authotization";
+    public static final String CONTENT_TYPE_JSON = "apllication/json";
+    public static final String CONTENT_TYPE_XML = "apllication/xml";
+    public static final String CONTENT_TYPE_YML = "apllication/x-yaml";
+    public static final String ORIGIN_ERUDIO = "https://erudio.com.br";
+    public static final String ORIGIN_SEMERU = "https://semeru.com.br";
+
+
 }
---
+++ src/test/java/br/com/erudio/apigateway/integrationtests/vo/PersonVO.java
@@ -0,0 +1,76 @@
+package br.com.erudio.apigateway.integrationtests.vo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.github.dozermapper.core.Mapping;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PersonVO implements Serializable {
+
+    private static final long serialVesionUID = 1L;
+
+
+    private Long id;
+    private String firtName;
+    private String lastName;
+    private String address;
+    private String gender;
+
+    public PersonVO(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirtName() {
+        return firtName;
+    }
+
+    public void setFirtName(String firtName) {
+        this.firtName = firtName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonVO personVO = (PersonVO) o;
+        return Objects.equals(id, personVO.id) && Objects.equals(firtName, personVO.firtName) && Objects.equals(lastName, personVO.lastName) && Objects.equals(address, personVO.address) && Objects.equals(gender, personVO.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firtName, lastName, address, gender);
+    }
+}
